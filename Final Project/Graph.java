fa87da7d91c50e07ba56e9a97474ec6dd466337b
Moz Alhindi <Moz Alhindi>
2016-12-01 23:11:01 -0500

    Broken code on graph and graphics, added more buisness logic on planning

--- Final Project/Graph.java
+++ Final Project/Graph.java
@@ -1,9 +1,42 @@
 //Graph Panel
 
+import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class Graph extends JPanel{
+   
+   //Data points for Graphs
+   
+   
+   //Vars
+   private int width;
+   private int height;
+   private int unitSeperation;
+   private int border;
+   
+   //Java GUI componenets
+   private JTextField xAxisLabel;
+   private JTextField yAxisLabel;
+   
+
+   public Graph(){
+      //setup drawing
+       //getContentPane().setBackground(Color.white);
+   
+      //inialize GUI Componenets
+      xAxisLabel = new JTextField("Proceding Profit/Expense");
+      yAxisLabel = new JTextField("Total Income");
+      
+      
+      setSize(500, 500);
+   }
+   
+   public void paint(Graphics g){
+      super.paint(g);
+   }
+   
+   
 
 }
--- Final Project/Graphics.java
+++ Final Project/Graphics.java
@@ -9,12 +9,25 @@ import java.awt.event.*;
 public class Graphics extends JFrame{
 
    public Graphics(){
+      
+      
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      pack();
+      setVisible(true);
+   }
    
+   private class AddButtonListener implements ActionListener{
+   
+      public void actionPreformed(ActionEvent e){
+      
+      }
    
    }
    
    
    public static void main(String[] args){
+      InputData backEnd = new InputData();
+      Graph dataGraph = new Graph();
       Graphics program = new Graphics();
       
       
